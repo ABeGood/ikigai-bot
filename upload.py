8585b882b3b0ae53223962340868be10a49a2469
ABeGood <openzzggl@gmail.com>
2024-09-01 22:12:41 +0200

    456

--- upload.py
+++ upload.py
@@ -36,24 +36,16 @@ def get_credentials():
     Returns:
         Credentials, the obtained credential.
     """
-    home_dir = '/app'
-    credential_dir = os.path.join(home_dir, '.credentials')
-    if not os.path.exists(credential_dir):
-        os.makedirs(credential_dir)
-    credential_path = os.path.join(credential_dir,
-                                   'ikigai-db-credentials.json')
+    # home_dir = '/app'
+    # credential_dir = os.path.join(home_dir, '.credentials')
+    # if not os.path.exists(credential_dir):
+    #     os.makedirs(credential_dir)
+    # credential_path = os.path.join(credential_dir,
+    #                                'ikigai-db-credentials.json')
 
     # store = Storage(credential_path)
-    store = Storage('ikigai-db-credentials.json')
+    store = Storage(CLIENT_SECRET_FILE)
     credentials = store.get()
-    if not credentials or credentials.invalid:
-        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
-        flow.user_agent = APPLICATION_NAME
-        if flags:
-            credentials = tools.run_flow(flow, store, flags)
-        else:  # Needed only for compatibility with Python 2.6
-            credentials = tools.run(flow, store)
-        print('Storing credentials to ' + credential_path)
     return credentials
 
 
